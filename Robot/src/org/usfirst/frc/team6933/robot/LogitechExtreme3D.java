44a253c7f4345fde568e13f423c66c5de282ac7d
Dan Leavitt <dan@leavitts.us>
2018-02-03 20:40:45 -0500

    Add Class to describe the logitech extreme 3d joystick.  We are currently using the gamepad definition with the extreme 3d pro joystick so confusion is inevitable.

---
+++ Robot/src/org/usfirst/frc/team6933/robot/LogitechExtreme3D.java
@@ -0,0 +1,171 @@
+package org.usfirst.frc.team6933.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+/**
+ * A Joystick definition for the Logitech Gamepad F310
+ * 
+ * Use this class instead of Joystick when you are using a Logitech F310.
+ * This class has definitions for every button and axis on the controller
+ * 
+ * @author vtleavs
+ *
+ */
+public class LogitechExtreme3D extends Joystick
+{	
+	
+	
+	
+	public static final int DPAD_OFF = -1;
+	public static final int DPAD_N = 0;
+	public static final int DPAD_NE = 45;
+	public static final int DPAD_E = 90;
+	public static final int DPAD_SE = 135;
+	public static final int DPAD_S = 180;
+	public static final int DPAD_SW = 225;
+	public static final int DPAD_W = 270;
+	public static final int DPAD_NW = 315;
+		
+	int XAxis = 0; 
+	int YAxis = 1;
+	int ZAxis = 2;	
+	
+	int throttleAxis = 3;
+	
+	int DPadAxis = 4;
+			
+	JoystickButton Trigger = new JoystickButton(this, 1);
+	JoystickButton SidePickle = new JoystickButton(this, 2);
+	JoystickButton ThumbButtonLowerLeft = new JoystickButton(this, 3);
+	JoystickButton ThumbButtonLowerRight = new JoystickButton(this, 4);
+	JoystickButton ThumbButtonUpperLeft = new JoystickButton(this, 5);
+	JoystickButton ThumbButtonUpperRight = new JoystickButton(this, 6);
+	JoystickButton BaseButtonUpperLeft = new JoystickButton(this, 7);
+	JoystickButton BaseButtonUpperRight = new JoystickButton(this, 8);
+	JoystickButton BaseButtonMiddleLeft = new JoystickButton(this, 9);
+	JoystickButton BaseButtonMiddleRight = new JoystickButton(this, 10);
+	JoystickButton BaseButtonBottomLeft = new JoystickButton(this, 11);
+	JoystickButton BaseButtonBottomRight = new JoystickButton(this, 12);
+		
+	public LogitechExtreme3D(int port)
+	{
+		super(port);
+	}
+	
+	/**
+	 * Gets the current value of the Extreme 3D Pro DPad
+	 * 
+	 * @return An integer 0-8 where:
+	 * <ul>
+	 * 		<li>0 = OFF</li>
+	 * 		<li>1 = N,</li>
+	 * 		<li>2 = NE,</li>
+	 * 		<li>3 = E,</li>
+	 * 		<li>4 = SE,</li>
+	 * 		<li>5 = S,</li>
+	 * 		<li>6 = SW,</li>
+	 * 		<li>7 = W,</li>
+	 * 		<li>8 = NW</li>
+	 * </ul>
+	 */
+	public int getDPad()
+	{
+		int pov = this.getPOV(0);
+		switch (pov)
+		{
+			case DPAD_OFF: return 0;
+			case DPAD_N: return 1;
+			case DPAD_NE: return 2;
+			case DPAD_E: return 3;
+			case DPAD_SE: return 4;
+			case DPAD_S: return 5;
+			case DPAD_SW: return 6;
+			case DPAD_W: return 7;
+			case DPAD_NW: return 8;
+		}
+		return -1;
+	}
+
+	/**
+	 * Gets the x-axis 
+	 * 
+	 * @return The double value of the axis
+	 */
+	public double getXAxis() {
+		return this.getRawAxis(XAxis);
+	}
+	
+	/**
+	 * Gets the y-axis 
+	 * 
+	 * @return The double value of the axis
+	 */
+	public double getYAxis() {
+		return -this.getRawAxis(YAxis);
+	}
+	
+	/**
+	 * Gets the x-axis 
+	 * 
+	 * @return The double value of the axis
+	 */
+	public double getZAxis() {
+		return this.getRawAxis(ZAxis);
+	}
+
+	public int getThrottleAxis() {
+		return throttleAxis;
+	}
+
+	public int getDPadAxis() {
+		return DPadAxis;
+	}
+
+	public JoystickButton getSidePickle() {
+		return SidePickle;
+	}
+
+	public JoystickButton getThumbButtonLowerLeft() {
+		return ThumbButtonLowerLeft;
+	}
+
+	public JoystickButton getThumbButtonLowerRight() {
+		return ThumbButtonLowerRight;
+	}
+
+	public JoystickButton getThumbButtonUpperLeft() {
+		return ThumbButtonUpperLeft;
+	}
+
+	public JoystickButton getThumbButtonUpperRight() {
+		return ThumbButtonUpperRight;
+	}
+
+	public JoystickButton getBaseButtonUpperLeft() {
+		return BaseButtonUpperLeft;
+	}
+
+	public JoystickButton getBaseButtonUpperRight() {
+		return BaseButtonUpperRight;
+	}
+
+	public JoystickButton getBaseButtonMiddleLeft() {
+		return BaseButtonMiddleLeft;
+	}
+
+	public JoystickButton getBaseButtonMiddleRight() {
+		return BaseButtonMiddleRight;
+	}
+
+	public JoystickButton getBaseButtonBottomLeft() {
+		return BaseButtonBottomLeft;
+	}
+
+	public JoystickButton getBaseButtonBottomRight() {
+		return BaseButtonBottomRight;
+	}
+	
+	
+}
+
